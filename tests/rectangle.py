0b8289131948fb6575f5b44a7f34e8ee639f192c
frank-gr588 <stephan.grigorchuk@gmail.com>
2024-12-12 08:24:50 +0300

    without error

--- tests/rectangle.py
+++ tests/rectangle.py
@@ -9,7 +9,7 @@ def perimeter(a, b):
 class RectangleTestCase(unittest.TestCase):
     def test_area(self):
         self.assertEqual(area(0, 0), 0)
-        self.assertEqual(area(5, 10), 40)
+        self.assertEqual(area(5, 10), 50)
         self.assertEqual(area(3, 7), 21)
 
     def test_perimeter(self):
